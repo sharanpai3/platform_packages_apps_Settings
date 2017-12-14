041ed670bbc3d4418d3637939c7959a93f52841b
ezio84 <brabus84@gmail.com>
2017-12-14 07:57:46 +0100

    Enable notifications led light by default [2/2]
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>
    Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com>

--- src/com/android/settings/notification/app/LightsPreferenceController.java
+++ src/com/android/settings/notification/app/LightsPreferenceController.java
@@ -91,7 +91,7 @@ public class LightsPreferenceController extends NotificationPreferenceController
             return false;
         }
         return Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.NOTIFICATION_LIGHT_PULSE, 0) == 1;
+                Settings.System.NOTIFICATION_LIGHT_PULSE, 1) == 1;
     }
 
     private void showLedPreview() {
