0dc2771fb55b613a4797edabef02a2586c6780a0
Dil3mm4 <Dil3mm4@users.noreply.github.com>
2021-05-23 13:12:56 +0530

    SettingsActivity: set actionbar only if null, requesting window feature only if non-null

--- src/com/android/settings/core/SettingsBaseActivity.java
+++ src/com/android/settings/core/SettingsBaseActivity.java
@@ -1,18 +1,8 @@
-/**
- * Copyright (C) 2018 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/** * Copyright (C) 2018 The Android Open Source Project * * Licensed under the Apache License, Version 2.0 (the "License"); * 
+ you may not use this file except in compliance with the License. * You may obtain a copy of the License at * * 
+ http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * 
+ distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
 package com.android.settings.core;
 
 import android.annotation.LayoutRes;
@@ -88,11 +78,13 @@ public class SettingsBaseActivity extends FragmentActivity {
         super.setContentView(R.layout.settings_base_layout);
 
         final Toolbar toolbar = findViewById(R.id.action_bar);
-        if (theme.getBoolean(android.R.styleable.Theme_windowNoTitle, false)) {
+        if (theme.getBoolean(android.R.styleable.Theme_windowNoTitle, false)  && getActionBar() != null) {
             toolbar.setVisibility(View.GONE);
             return;
         }
-        setActionBar(toolbar);
+	if (getActionBar() == null) setActionBar(toolbar);
+
+/*        setActionBar(toolbar);
 
         /*baseSpacer = findViewById(R.id.settings_submenu_spacer);
         baseMainLayout = findViewById(R.id.base_content_scrollable_container);
